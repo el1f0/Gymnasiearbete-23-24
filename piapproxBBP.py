4ef8a03d5890ad1d702a9ad360ac2d9b973bdd54
el1f0 <152384120+el1f0@users.noreply.github.com>
2024-05-15 20:33:11 +0200

    Update piapproxBBP.py
    - Added ability to time
    - Added looping to sample time across multiple passes
    - Halved the amount of calculations made

--- piapproxBBP.py
+++ piapproxBBP.py
@@ -1,30 +1,39 @@
 import math
 import decimal
-
-def sum(n):
-    pi=decimal.Decimal(0)
-    for i in range (n+1):
-        decimal.getcontext().prec=math.ceil(n**(1+1/n))
-        pi+=decimal.Decimal(1)/(16**i)*(decimal.Decimal(4)/(8*i+1)-decimal.Decimal(2)/(8*i+4)-decimal.Decimal(1)/(8*i+5)-decimal.Decimal(1)/(8*i+6))
-    pi=str(pi)
-    nypi=decimal.Decimal(3)
-    piList=[*pi]
-    piList.pop(0)
-    piList.pop(0)
-    delta=math.ceil(n**(1+1/n))-1-n
-    for i in range (delta):
-        piList.pop(len(piList)-1)
-    for i in range (len(piList)):
-        piList[i]=decimal.Decimal(piList[i])
-        nypi+=piList[i]/(10**(1+i))
-    global y
-    y = len(str(nypi))-2
-    return str(nypi)
+import time
 digit =int(input("what decimal? "))
+antalupprepningar = int(input("hur många upprepningar? "))
+st=time.time()
+def sum(n, z):
+    for x in range (z+1):
+        st1=time.time()
+        pi=decimal.Decimal(0)
+        for i in range (n+1):
+            decimal.getcontext().prec=math.ceil(n**(1+1/n))
+            pi+=decimal.Decimal(1)/(16**i)*(decimal.Decimal(4)/(8*i+1)-decimal.Decimal(2)/(8*i+4)-decimal.Decimal(1)/(8*i+5)-decimal.Decimal(1)/(8*i+6))
+        pi=str(pi)
+        nypi=decimal.Decimal(3)
+        piList=[*pi]
+        piList.pop(0)
+        piList.pop(0)
+        delta=math.ceil(n**(1+1/n))-1-n
+        for i in range (delta):
+            piList.pop(len(piList)-1)
+        for i in range (len(piList)):
+            piList[i]=decimal.Decimal(piList[i])
+            nypi+=piList[i]/(10**(1+i))
+        global y
+        global result
+        result =str(nypi)
+        y = len(str(nypi))-2
+        et1=time.time()
+        print("Loop ", x, " time elapsed: ", et1-st1)
+sum(digit, antalupprepningar)
 with open('piapproxBBP.txt', 'w') as f:
-    f.write(sum(digit))
+    f.write(result)
     f.close()
-print(sum(digit))
+print(result)
 print(y)
-
+et=time.time()
+print("time elapsed: ", (et-st)/(antalupprepningar+1))
 
